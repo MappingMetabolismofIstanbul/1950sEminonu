b84f31fb90da669cbf0bb8101c34026c4a0d6198
MappingMetabolismofIstanbul <dogatmn@gmail.com>
2025-08-14 20:47:11 +0300

    Update config.js

--- config.js
+++ config.js
@@ -16,10 +16,10 @@ var config = {
     theme: 'light',
     use3dTerrain: false, //set true for enabling 3D maps.
     auto: false,
-    title: '1950’ler İstanbul’unda İmar ve Yıkım Süreci / Urban Planning and Demolition Process in 1950s Istanbul',
+    title: '1950’ler İstanbul’unda İmar ve Yıkım Süreci / Urban Production and Demolition Process in 1950s Istanbul',
     subtitle: '',
     byline: 'BAP_Mapping Metabolism of Istanbul_Interactive Mapping / MEF AAP_027',
-    footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
+    footer: 'BAP_Mapping Metabolism of Istanbul_Interactive Mapping / MEF AAP_027',
     chapters: [
         {
             id: '1',
@@ -163,7 +163,7 @@ var config = {
                 //curve: 0, // change the speed at which it zooms out
             },
             mapAnimation: 'flyTo',
-            rotateAnimation: true,
+            rotateAnimation: false,
             callback: '',
             onChapterEnter: [],
             onChapterExit: []
@@ -176,8 +176,8 @@ var config = {
             image: 'https://i.ibb.co/bRGhGf9V/5.png',
             description: '',
             location: {
-                center: [28.97131817837604,41.018050376587205],
-                zoom: 19,
+                 center: [28.974963057992756,41.01690271971029],
+                zoom: 15.7,
                 pitch: 10,
                 bearing: 0,
                 speed: 1,
@@ -189,38 +189,12 @@ var config = {
                 //speed: 2, // make the flying slow
                 //curve: 0, // change the speed at which it zooms out
             },
-            mapAnimation: 'right',
-            rotateAnimation: true,
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
             callback: '',
             onChapterEnter: [],
             onChapterExit: []
         },
-         {
-            id: '6',
-            alignment: 'right',
-            hidden: false,
-            title: '',
-            image: '',
-            description: '',
-            location: {
-                center: [28.97131817837604,41.018050376587205],
-                zoom: 19,
-                pitch: 10,
-                bearing: 0,
-                speed: 1,
-                curve: 2
-                // flyTo additional controls-
-                // These options control the flight curve, making it move
-                // slowly and zoom out almost completely before starting
-                // to pan.
-                //speed: 2, // make the flying slow
-                //curve: 0, // change the speed at which it zooms out
-            },
-            mapAnimation: 'right',
-            rotateAnimation: true,
-            callback: '',
-            onChapterEnter: [],
-            onChapterExit: []
-        },
+         
     ]
 };
